63b84bad4fe8bc1620b8cbec4659a65006e91bed
Tapani Viljanen <tapani.viljanen@gmail.com>
2014-01-31 14:44:03 +0200

    small amount bugs fixed,,,

--- cruncher.py
+++ cruncher.py
@@ -13,10 +13,9 @@ def crunch(mtext):
         x, y = mop.span()
         moc = mocalc(mtext[x + 1: y])
         if moc is not None:     # calculations found ?
-            tmp = calc(moc.group())  # recursion
-            x, y = moc.span()
-            mtext = '%s%s%s' % (mtext[:x], tmp, mtext[y:])
-            return crunch(mtext)
+            tmp = moc.group()
+            tmp = crunch(tmp)
+            mtext = '%s%s%s' % (mtext[:x+1], tmp, mtext[y-1:])
         moc = mofun(mtext)
         if moc is not None:
             tmp = eval(mangle_fun_to_exec(moc.group()))
@@ -25,14 +24,14 @@ def crunch(mtext):
             return crunch(mtext)
         moc = mocon(mtext)
         if moc is not None:
-            print (moc.group())
             tmp = eval(mangle_const_to_exec(moc.group()))
-            x, y = moc.span()
-            mtext = '%s%s%s' % (mtext[:x], tmp, mtext[y:])
+            i, j = moc.span()
+            mtext = '%s%s%s' % (mtext[:i], tmp, mtext[j:])
             return crunch(mtext)
+         # remove dummy parenthesis
+        mtext = '%s%s%s' % (mtext[:x], mtext[x+1:y-1], mtext[y:])
     moc = mocalc(mtext)
     if moc is not None:
-        print (moc.group())
         tmp = eval(mangle_calc_to_exec(moc.group()))
         x, y = moc.span()
         mtext = '%s%s%s' % (mtext[:x], tmp, mtext[y:])
